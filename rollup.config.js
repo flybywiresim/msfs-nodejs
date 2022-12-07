30db3ae9994fabe3135cd34c4fecb13bb4fc41aa
Sven Czarnian <devel@svcz.de>
2022-12-07 23:24:26 +0100

    code formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,29 @@
-"use strict";
+'use strict';
 
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
 
-import packageJson from "./package.json";
+import packageJson from './package.json';
 
 export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    commonjs(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      exclude: ["**/*.test.ts", "**/*.spec.ts"]
-    })
-  ]
+    input: 'src/index.ts',
+    output: [
+        {
+            file: packageJson.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: packageJson.module,
+            format: 'esm',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        commonjs(),
+        typescript({
+            useTsconfigDeclarationDir: true,
+            exclude: ['**/*.test.ts', '**/*.spec.ts'],
+        }),
+    ],
 };

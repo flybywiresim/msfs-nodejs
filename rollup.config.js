78a494aff6fec380eb27064326edb1a1d20847fb
Sven Czarnian <devel@svcz.de>
2022-12-08 14:00:31 +0100

    rollup the binding

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import nativePlugin from 'rollup-plugin-natives';
 
 import packageJson from './package.json';
 
@@ -24,6 +25,14 @@ export default {
         typescript({
             useTsconfigDeclarationDir: true,
             exclude: ['**/*.test.ts', '**/*.spec.ts'],
+        }),
+        nativePlugin({
+            copyTo: 'dist/libs',
+            destDir: './libs',
+            dlopen: false,
+            map: (_modulePath) => 'msfs.node',
+            sourcemap: true,
+            targetEsm: false,
         }),
     ],
 };

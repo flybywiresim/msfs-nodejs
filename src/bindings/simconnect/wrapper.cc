afa9963df9366987477053748dce351095b7e66a
Sven Czarnian <devel@svcz.de>
2022-12-11 00:28:21 +0100

    handle the client data areas

--- src/bindings/simconnect/wrapper.cc
+++ src/bindings/simconnect/wrapper.cc
@@ -8,10 +8,43 @@ using namespace msfs::simconnect;
 Wrapper::Wrapper(const Napi::CallbackInfo& info) :
         Napi::ObjectWrap<Wrapper>(info),
         _simConnect(0),
+        _clientDataAreas(),
         _lastError() { }
 
 Wrapper::~Wrapper() {
     this->close();
+}
+
+bool Wrapper::clientDataIdExists(SIMCONNECT_CLIENT_DATA_ID clientDataId) const {
+    for (const auto& dataArea : std::as_const(this->_clientDataAreas)) {
+        if (dataArea.first == clientDataId)
+            return true;
+    }
+
+    return false;
+}
+
+Napi::Value Wrapper::newClientDataArea(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    if (info.Length() != 1) {
+        Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    if (!info[0].IsNumber()) {
+        Napi::TypeError::New(env, "Invalid argument type. 'clientDataId' must be a number").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    const SIMCONNECT_CLIENT_DATA_ID clientDataId = info[0].As<Napi::Number>().Uint32Value();
+
+    if (this->clientDataIdExists(clientDataId) == true) {
+        this->_lastError = "Client data ID is already in use";
+        return Napi::Boolean::New(env, false);
+    }
+
+    this->_clientDataAreas.insert({ clientDataId, {} });
+    return Napi::Boolean::New(env, true);
 }
 
 Napi::Value Wrapper::lastError(const Napi::CallbackInfo& info) {
@@ -25,9 +58,11 @@ Napi::Value Wrapper::createNewItem(const Napi::CallbackInfo& info) {
 }
 
 Napi::Object Wrapper::initialize(Napi::Env env, Napi::Object exports) {
-    Napi::Function func = DefineClass(env, "SimConnect", {
+    Napi::Function func = DefineClass(env, "Wrapper", {
         InstanceMethod<&Wrapper::open>("open", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::close>("close", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
+        InstanceMethod<&Wrapper::newClientDataArea>("newClientDataArea", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
+        InstanceMethod<&Wrapper::createClientDataArea>("createClientDataArea", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::lastError>("lastError", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         StaticMethod<&Wrapper::createNewItem>("createNewItem", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
     });
@@ -35,7 +70,7 @@ Napi::Object Wrapper::initialize(Napi::Env env, Napi::Object exports) {
     Napi::FunctionReference* constructor = new Napi::FunctionReference();
 
     *constructor = Napi::Persistent(func);
-    exports.Set("SimConnect", func);
+    exports.Set("Wrapper", func);
 
     env.SetInstanceData<Napi::FunctionReference>(constructor);
 

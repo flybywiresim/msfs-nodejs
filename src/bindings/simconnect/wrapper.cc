f96da42a4f37cb35bc87ac3a7e51c3eb5d83af2d
Sven Czarnian <devel@svcz.de>
2022-12-12 11:23:56 +0100

    export the clear function

--- src/bindings/simconnect/wrapper.cc
+++ src/bindings/simconnect/wrapper.cc
@@ -67,6 +67,7 @@ Napi::Object Wrapper::initialize(Napi::Env env, Napi::Object exports) {
         InstanceMethod<&Wrapper::newClientDataArea>("newClientDataArea", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::mapClientDataNameToId>("mapClientDataNameToId", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::addClientDataDefinition>("addClientDataDefinition", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
+        InstanceMethod<&Wrapper::clearClientDataDefinition>("clearClientDataDefinition", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::createClientDataArea>("createClientDataArea", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::setClientData>("setClientData", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
         InstanceMethod<&Wrapper::lastError>("lastError", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),

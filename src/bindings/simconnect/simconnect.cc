3571801f925200b3a4455a71e634b7bb8b666973
Sven Czarnian <devel@svcz.de>
2022-12-14 11:02:25 +0100

    remove the wrapper

--- src/bindings/simconnect/simconnect.cc
+++ src/bindings/simconnect/simconnect.cc
@@ -1,14 +1,12 @@
 #include <napi.h>
 
 #include "clientdataarea.h"
-#include "wrapper.h"
 
 using namespace msfs::simconnect;
 
 void test(const Napi::CallbackInfo& info) { }
 
 Napi::Object initialize(Napi::Env env, Napi::Object exports) {
-    exports = Wrapper::initialize(env, exports);
     exports = Connection::initialize(env, exports);
     exports = ClientDataArea::initialize(env, exports);
     return exports;

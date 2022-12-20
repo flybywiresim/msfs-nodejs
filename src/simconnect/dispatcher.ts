58b9383bd00caf2cce4d42faff94fddd724871b6
Sven Czarnian <devel@svcz.de>
2022-12-20 20:49:01 +0100

    export the request function for simulator data

--- src/simconnect/dispatcher.ts
+++ src/simconnect/dispatcher.ts
@@ -1,12 +1,14 @@
 import { ClientDataArea } from './clientdataarea';
-import { ClientDataPeriod, ClientDataRequest } from './constants';
+import { ClientDataPeriod, ClientDataRequest, SimulatorDataPeriod } from './constants';
 import { Connection } from './connection';
+import { SimulatorDataArea } from './simulatordataarea';
 import { DispatcherResponse } from './types';
 
 const simconnect = require('./libs/simconnect');
 
 export interface Dispatcher {
     requestClientData(clientData: ClientDataArea, period: ClientDataPeriod, request: ClientDataRequest): boolean;
+    requestSimulatorData(simulatorData: SimulatorDataArea, period: SimulatorDataPeriod): boolean;
     nextDispatch(): DispatcherResponse;
     lastError(): string;
 }

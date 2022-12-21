ba517fae7921763009efe4f0b750909dbb5c1864
Sven Czarnian <devel@svcz.de>
2022-12-21 11:46:04 +0100

    remove dist files

--- dist/simconnect/dataareas/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './clientdataarea';
-export * from './simulatordataarea';
--- dist/simconnect/dataareas/simulatordataarea.d.ts
+++
@@ -1,9 +0,0 @@
-import { Connection } from '../connection';
-import { SimulatorDataDefinition } from '../types';
-export interface SimulatorDataArea {
-    addDataDefinition(dataDefinition: SimulatorDataDefinition): boolean;
-    lastError(): string;
-}
-export declare const SimulatorDataArea: {
-    new (connection: Connection, simulatorDataId: number): SimulatorDataArea;
-};

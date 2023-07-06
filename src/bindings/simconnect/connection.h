fbbc99b1d2414dff574b4cf4331ad4280a8b9e6e
Sven Czarnian <devel@svcz.de>
2023-07-06 22:18:50 +0200

    add the system event id management to the connection

--- src/bindings/simconnect/connection.h
+++ src/bindings/simconnect/connection.h
@@ -19,6 +19,7 @@ namespace simconnect {
         std::string _lastError;
         std::list<SIMCONNECT_CLIENT_DATA_ID> _clientDataIds;
         std::list<SIMCONNECT_DATA_DEFINITION_ID> _simulatorDataIds;
+        std::list<std::uint32_t> _systemEventIds;
 
         void close();
 
@@ -58,6 +59,17 @@ namespace simconnect {
          * @param simulatorDataId The simulator data ID
          */
         void addSimulatorDataId(SIMCONNECT_DATA_DEFINITION_ID simulatorDataId);
+        /**
+         * @brief Checks if a system event ID exists
+         * @param systemEventId The system event ID
+         * @return True if the ID is already registered, else false
+         */
+        bool systemEventIdExists(std::uint32_t systemEventId) const;
+        /**
+         * @brief Adds the system event ID to the managed list
+         * @param systemEventId The system event ID
+         */
+        void addSystemEventId(std::uint32_t systemEventId);
         /**
          * @brief Marks if the connection with the server response is established
          * @param established True if the server response was received, else false
